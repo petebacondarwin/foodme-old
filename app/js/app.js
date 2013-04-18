7b43e0eedfe77f7c291324a5967db7d2d83098d0
Pete Bacon Darwin <pete@bacondarwin.com>
2013-04-18 13:57:53 +0100

    Step 6 - Solution

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,9 @@
-angular.module('foodMeApp', ['customer']);
+angular.module('foodMeApp', ['customer'])
+
+.config(function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      controller: 'CustomerController',
+      templateUrl: 'views/customer.html'
+    });
+});
