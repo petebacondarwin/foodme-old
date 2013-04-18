265dbc54da4c07d036b50fc1bb0953af9258bff8
Pete Bacon Darwin <pete@bacondarwin.com>
2013-04-18 13:49:46 +0100

    Step 4 - Customer model, localStorage persistence

---
+++ test/unit/models/customer.spec.js
@@ -0,0 +1,34 @@
+describe('customer model', function() {
+  var customer, localStorage, $rootScope;
+
+  beforeEach(module('foodMeApp'));
+  beforeEach(module(function($provide) {
+    localStorage = {
+      fmCustomer: '{"name":"init-name","address":"init-address"}'
+    };
+
+    $provide.value('localStorage', localStorage);
+  }));
+
+  beforeEach(inject(function(_customer_, _$rootScope_) {
+    customer = _customer_;
+    $rootScope = _$rootScope_;
+  }));
+
+
+  it('should update any change to localStorage', function() {
+    $rootScope.$apply(function() {
+      customer.name = 'Michael Jackson';
+      customer.address = '2231 Planet Mars, Apt 501';
+    });
+
+    expect(localStorage.fmCustomer).toBe('{"name":"Michael Jackson",' +
+      '"address":"2231 Planet Mars, Apt 501"}');
+  });
+
+
+  it('should load initial value from localStorage', function() {
+    expect(customer.name).toBe('init-name');
+    expect(customer.address).toBe('init-address');
+  });
+});
