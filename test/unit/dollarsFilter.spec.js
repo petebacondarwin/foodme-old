b28ba2272f96c2d49d9bd1c5c2a863e499f97e53
Pete Bacon Darwin <pete@bacondarwin.com>
2013-04-18 14:14:56 +0100

    Step 20 - stars & dollars filters

---
+++ test/unit/dollarsFilter.spec.js
@@ -0,0 +1,6 @@
+describe('filters dollars', function() {
+  it('should show proper number of dollars', inject(function(dollarsFilter) {
+    expect(dollarsFilter(1)).toBe('$');
+    expect(dollarsFilter(3)).toBe('$$$');
+  }));
+});
