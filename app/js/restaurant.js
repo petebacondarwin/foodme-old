8a3a14c4b4f5c66c22d218f6d2d6a38bd38d26a3
Pete Bacon Darwin <pete@bacondarwin.com>
2013-04-18 14:23:56 +0100

    Step 24 - Solution

--- app/js/restaurant.js
+++ app/js/restaurant.js
@@ -12,6 +12,11 @@ angular.module('restaurant', ['customer', 'ngResource', 'fmDeliverTo', 'fmRating
     cuisine: []
   };
 
+  $scope.sort = {
+    key: null,
+    asc: false
+  };
+
   $scope.CUISINE_OPTIONS = {
     african: 'African',
     american: 'American',
@@ -32,10 +37,11 @@ angular.module('restaurant', ['customer', 'ngResource', 'fmDeliverTo', 'fmRating
     $location.url('/customer');
   }
 
-  var allRestaurants = Restaurant.query(filterRestaurants);
-  $scope.$watch('filter', filterRestaurants, true);
+  var allRestaurants = Restaurant.query(filterAndSortRestaurants);
+  $scope.$watch('filter', filterAndSortRestaurants, true);
+  $scope.$watch('sort', filterAndSortRestaurants, true);
 
-  function filterRestaurants() {
+  function filterAndSortRestaurants() {
     $scope.restaurants = [];
 
     // filter
@@ -45,7 +51,36 @@ angular.module('restaurant', ['customer', 'ngResource', 'fmDeliverTo', 'fmRating
       if ($scope.filter.cuisine.length && $scope.filter.cuisine.indexOf(item.cuisine) === -1) { return; }
       $scope.restaurants.push(item);
     });
+
+    // sort
+    $scope.restaurants.sort(function(a, b) {
+      if (a[$scope.sort.key] > b[$scope.sort.key]) {
+        return $scope.sort.asc ? 1 : -1;
+      }
+
+      if (a[$scope.sort.key] < b[$scope.sort.key]) {
+        return $scope.sort.asc ? -1 : 1;
+      }
+
+      return 0;
+    });
   }
+
+  $scope.sortBy = function(key) {
+    if ($scope.sort.key === key) {
+      $scope.sort.asc = !$scope.sort.asc;
+    } else {
+      $scope.sort.key = key;
+      $scope.sort.asc = true;
+    }
+  };
+
+  $scope.sortIconFor = function(key) {
+    if ($scope.sort.key !== key) {
+      return '';
+    }
+    return $scope.sort.asc ? '\u25B2' : '\u25BC';
+  };
 })
 
 .controller('MenuController', function MenuController($scope, $routeParams, Restaurant) {
