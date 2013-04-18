344fce30c4c14877c8b053713ec853c821466371
Pete Bacon Darwin <pete@bacondarwin.com>
2013-04-18 14:12:32 +0100

    Step 18 - add fmDeliverTo directive

---
+++ test/unit/fmDeliverTo.spec.js
@@ -0,0 +1,39 @@
+describe('deliverTo directive', function() {
+  var customer, element;
+
+
+  // load template into $templateCache
+  beforeEach(module('app/js/fmDeliverTo/fmDeliverTo.template.html'));
+
+  beforeEach(module(function($provide){
+    customer = {};
+    $provide.value('customer', customer);
+  }));
+
+  beforeEach(inject(function($compile, $rootScope) {
+    element = $compile('<fm-deliver-to></fm-deliver-to>')($rootScope);
+    $rootScope.$apply();
+  }));
+
+
+  it("should display customer's address", inject(function($compile, $rootScope, customer) {
+    customer.address = "some really cool address";
+    $rootScope.$apply();
+
+    expect(element.text()).toMatch(/Deliver to: some really cool address/);
+
+    customer.address = "changed address";
+    $rootScope.$apply();
+
+    expect(element.text()).toMatch(/Deliver to: changed address/);
+  }));
+
+
+  it("should display a link to change the delivery address", inject(function($compile, $rootScope) {
+    var anchor = element.find('a');
+
+    expect(anchor.attr('href')).toBe('#/customer');
+    expect(anchor.text()).toBe('Change');
+  }));
+
+});
